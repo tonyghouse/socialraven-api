ce5e1e9481e9c7bf04238782e7db6cbf5dcec1fb
Tony Ghouse <tonyghouseofficial@gmail.com>
2026-03-26 23:10:40 +0530

    insta oauth fixed

--- src/main/java/com/ghouse/socialraven/service/provider/InstagramOAuthService.java
+++ src/main/java/com/ghouse/socialraven/service/provider/InstagramOAuthService.java
@@ -165,10 +165,11 @@ public class InstagramOAuthService {
 
     private Map<String, Object> exchangeForLongLivedToken(String shortAccessToken) {
         String url = UriComponentsBuilder
-                .fromHttpUrl("https://graph.instagram.com/access_token")
-                .queryParam("grant_type", "ig_exchange_token")
+                .fromHttpUrl("https://graph.facebook.com/oauth/access_token")
+                .queryParam("grant_type", "fb_exchange_token")
+                .queryParam("client_id", appId)
                 .queryParam("client_secret", appSecret)
-                .queryParam("access_token", shortAccessToken)
+                .queryParam("fb_exchange_token", shortAccessToken)
                 .toUriString();
 
         Map<String, Object> response = rest.exchange(
