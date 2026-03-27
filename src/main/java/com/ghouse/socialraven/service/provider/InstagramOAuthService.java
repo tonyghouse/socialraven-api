e231f09e419a598fad290ca311111b189243d213
Tony Ghouse <tonyghouseofficial@gmail.com>
2026-03-27 06:59:53 +0530

    import fixed

--- src/main/java/com/ghouse/socialraven/service/provider/InstagramOAuthService.java
+++ src/main/java/com/ghouse/socialraven/service/provider/InstagramOAuthService.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
